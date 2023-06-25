14607c7fe054ae40f65d593c6ff883507b39293b
MirzaMuhammadBaig <muhammadbaig090@gmail.com>
2023-06-25 12:09:14 +0500

    removed-error-from-buyticket

--- frontend/src/components/BuyTicket.tsx
+++ frontend/src/components/BuyTicket.tsx
@@ -1,9 +1,13 @@
 import { FC, useState } from "react";
 import { Button, TextField } from "@mui/material";
+import { BigNumber } from "@ethersproject/bignumber";
 
 import { BoxItem } from "./BoxItem";
 import { Zone } from "../constants";
-import { useCustomContractFunction, useGetZonePrice } from "../hooks/useContractHooks";
+import {
+  useCustomContractFunction,
+  useGetZonePrice,
+} from "../hooks/useContractHooks";
 import { ZoneSelect } from "./ZoneSelect";
 import { Toast } from "./Toast";
 
@@ -13,12 +17,14 @@ export const BuyTicket: FC = () => {
   const [zone, setZone] = useState<Zone | string>("");
   const [plate, setPlate] = useState<string>("");
   const [duration, setDuration] = useState<number>(60);
+  const [price, setPrice] = useState<BigNumber>(BigNumber.from(0));
+
   const zonePrice = useGetZonePrice(zone as Zone);
-  const totalPrice = zonePrice.mul(duration || 0);
+  const totalPrice = price || zonePrice.mul(duration || 0);
   const [tx, clearTx, buyTicket] = useCustomContractFunction("buyTicket");
 
   const handleBuyTicket = async () => {
-    if (!plate || !duration || zone === "") return;
+    if (!plate || !duration || zone === "" || totalPrice.lte(0)) return;
 
     clearTx();
     await buyTicket(plate, duration, zone as Zone, { value: totalPrice });
@@ -41,6 +47,15 @@ export const BuyTicket: FC = () => {
         value={duration}
         onChange={(e) => setDuration(parseInt(e.target.value))}
         inputProps={{ min: "1" }}
+      />
+      <TextField
+        style={style}
+        label="Price (in Wei):"
+        variant="outlined"
+        type="number"
+        value={price.toString()}
+        onChange={(e) => setPrice(BigNumber.from(e.target.value || 0))}
+        inputProps={{ min: "0" }}
       />
       <ZoneSelect zone={zone} setZone={setZone} />
       <Button variant="contained" color="secondary" onClick={handleBuyTicket}>
